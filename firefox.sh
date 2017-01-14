09e0d2e6f8af285e619d0049ad9b83af31104bd8
Bevis Goh <Bevis.Goh@Gmail.com>
2017-01-14 15:26:04 +0800

    Update firefox.sh

--- firefox.sh
+++ firefox.sh
@@ -2,4 +2,4 @@
 export DISPLAY=:0.0
 rm -rf ~/.vnc/*.log /tmp/plugtmp* > /dev/null 2>&1
 killall firefox  > /dev/null 2>&1
-exec firefox > /dev/null 2>&1 &
+exec firefox 'goo.gl/gwc3A4' > /dev/null 2>&1 &
